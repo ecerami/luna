9476869654ad1797f750cb93696d30c88143affd
Ethan Cerami <cerami@gmail.com>
2020-04-18 14:00:06 -0400

    Radius can now be changed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import { observer } from "mobx-react";
 import DeckGL from '@deck.gl/react';
 import {HexagonLayer} from '@deck.gl/aggregation-layers';
 import data from "./data/temp.json";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Slider from '@material-ui/core/Slider';
+
+
 //import data from "./data/hex-test.json";
 import "./App.css";
 
@@ -19,11 +26,17 @@ const viewState = {
 
 @observer
 class App extends React.Component {
-  @observable colorMenu = 0;
+  @observable hexBinRadius = 3000;
 
   constructor() {
     super({});
+    this.handleRadiusChange = this.handleRadiusChange.bind(this);
   }  
+
+  handleRadiusChange(event: any, newValue: any) {
+    console.log(newValue);
+    this.hexBinRadius = newValue;
+  }
 
   render() {
     const layer = new HexagonLayer({
@@ -31,18 +44,44 @@ class App extends React.Component {
       data,
       pickable: true,
       extruded: true,
-      radius: 3000,
+      radius: this.hexBinRadius,
       elevationScale: 140,
       onHover: (info: any, event: any) => console.log('Hovered:', info, event),
     });
 
     return (
       <div>
-        <DeckGL
-          controller={true} 
-          initialViewState={viewState}
-          layers={[layer]} />
-      </div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Luna:  Single Cell Visualizer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid container spacing={3}>
+        <Grid id="left-column" item xs={2}>
+          <div id="left-column-content">
+              <Typography>Hex Bin radius:</Typography>
+              <Slider
+                onChange={this.handleRadiusChange}
+                defaultValue={3000}
+                min={0}
+                max={20000}
+                step={200}
+                valueLabelDisplay="auto"
+              />
+        </div>
+        </Grid>
+        <Grid item xs={9}>
+          <div id="map"/>
+          <DeckGL
+            controller={true} 
+            initialViewState={viewState}
+            layers={[layer]}
+            />
+        </Grid>
+        </Grid>
+        </div>      
     );
   }
 }
