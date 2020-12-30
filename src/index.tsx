92c4466d5f8c7b1cf9fa7310cab4c02a75c0e987
Ethan Cerami <cerami@gmail.com>
2020-12-29 22:13:56 -0500

    Remove React strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import Main from './Main';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
+    <Main />,
   document.getElementById('root')
 );
 
